d6715a24ed41326a4b7187d30ea276a4f0b36925
japavell <laicans512@gmail.com>
2025-07-23 14:43:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function registrarEntrada() {
+  alert("✨ Entrada registrada! Você ganhou uma carta rara!");
+}
+function registrarSaida() {
+  alert("💸 Saída registrada! Você ganhou uma carta sombria...");
+}
